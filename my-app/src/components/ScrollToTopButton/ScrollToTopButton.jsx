7053527dc343de67ab2cbae73eac6dcc4373111e
An Nguyen <annguyenmxh@gmail.com>
2024-03-07 13:03:50 +0700

    1h03

--- my-app/src/components/ScrollToTopButton/ScrollToTopButton.jsx
+++ my-app/src/components/ScrollToTopButton/ScrollToTopButton.jsx
@@ -1,33 +1,18 @@
-import React, { useEffect, useState } from 'react';
-
-import { SlArrowUp } from "react-icons/sl";
-
-const ScrollToTopButton = () => {
-    const [isButtonVisible, setIsButtonVisible] = useState(true)
+import React from 'react'
+import toTop from '../../assets/product/totop.png'
+import './ScrollToTopButton.css'
+function ScrollToTopButton() {
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
-            behavior: 'smooth'
-        })
-    }
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-            setIsButtonVisible(scrollTop > 100);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-    }, []);
+            behavior: "smooth"
+        });
+    };
 
     return (
-        <button onClick={scrollToTop}
-            style={{ display: isButtonVisible ? "block" : "none" }}
-            className='fixed bottom-7 left-5 border-2 border-black rounded-[5px] text-black text-[13px] p-[0.9em] transition-all duration-300 ease-in-out hover:bg-[#b20000] hover:border-[#b20000] hover:text-white'
-        >
-            <SlArrowUp />
-        </button>
-    )
-
+        <a className='totop' href='#' onClick={scrollToTop}>
+            <img src={toTop} alt="" />
+        </a>
+    );
 }
-
-export default ScrollToTopButton;
+export default ScrollToTopButton
