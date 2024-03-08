5a9043091d1fec2c29ba03f5b6a135ec2925b254
Ngo Manh Thong <ngomanhthong0812>
2024-03-08 13:13:25 +0700

    Update Product Tabs

--- my-app/src/components/ProductTabs/ProductTabs.jsx
+++ my-app/src/components/ProductTabs/ProductTabs.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
 
 import Product from "../Categories/Product";
-import { db_topBanChay } from "../Categories/db/db_topBanChay";
-import { db_moHinhDep } from "../Categories/db/db_moHinhDep";
-import { db_tranhCanva } from "../Categories/db/db_tranhCanva";
-import { db_vaiDecor } from "../Categories/db/db_vaiDecor";
-import { db_setDecal } from "../Categories/db/db_setDecal";
+import { db_product } from "../Categories/db/db_product";
 import { TabItems } from './TabItems'
 import './ProductTabs.css'
 
@@ -26,11 +22,11 @@ const ProductTabs = () => {
 
             </div>
             <div className="content-tabs p-7">
-                <div className={toggleState === 1 ? "active-content" : "content"}> <Product productList={db_topBanChay} /></div>
-                <div className={toggleState === 2 ? "active-content" : "content"}> <Product productList={db_moHinhDep} /></div>
-                <div className={toggleState === 3 ? "active-content" : "content"}> <Product productList={db_tranhCanva} /></div>
-                <div className={toggleState === 4 ? "active-content" : "content"}> <Product productList={db_vaiDecor} /></div>
-                <div className={toggleState === 5 ? "active-content" : "content"}> <Product productList={db_setDecal} /></div>
+                <div className={toggleState === 1 ? "active-content" : "content"}> <Product productList={db_product} categories={"top-ban-chay"} quantity={8} /></div>
+                <div className={toggleState === 2 ? "active-content" : "content"}> <Product productList={db_product} categories={"mo-hinh-dep"} quantity={8} /></div>
+                <div className={toggleState === 3 ? "active-content" : "content"}> <Product productList={db_product} categories={"tranh-canva"} quantity={8} /></div>
+                <div className={toggleState === 4 ? "active-content" : "content"}> <Product productList={db_product} categories={"vai-decor"} quantity={8} /></div>
+                <div className={toggleState === 5 ? "active-content" : "content"}> <Product productList={db_product} categories={"set-decal"} quantity={8} /></div>
             </div>
 
         </div>
